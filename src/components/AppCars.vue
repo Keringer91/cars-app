c92d3077d34139ea8f7923a3fe72ad4e0ae6bf56
Sabolc Keringer <keringersz@gmail.com>
2019-11-26 19:46:17 +0100

    Zadatak 1: Layout created, cars page added with routing

---
+++ src/components/AppCars.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+   <h3>All Cars</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppCars',
+  props: {
+    
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
+import VueRouter from "vue-router";
 import App from './App.vue'
+import AppCars from './components/AppCars.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter);
+
+const routes = [
+  { name: "index", path: "/", redirect: "/cars" },
+  { name: "cars", path: "/cars", component: AppCars },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+});
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: h => h(App)
+}).$mount("#app");
